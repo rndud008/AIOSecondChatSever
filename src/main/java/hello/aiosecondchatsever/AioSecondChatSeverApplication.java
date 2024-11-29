a11a0555e78aa29797d6feb7457b301cb3633c08
rndud008 <rndud008@gamil.com>
2024-11-29 20:44:22 +0900

    init

---
+++ src/main/java/hello/aiosecondchatsever/AioSecondChatSeverApplication.java
@@ -0,0 +1,13 @@
+package hello.aiosecondchatsever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AioSecondChatSeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AioSecondChatSeverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/aiosecondchatsever/AioSecondChatSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.aiosecondchatsever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AioSecondChatSeverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
